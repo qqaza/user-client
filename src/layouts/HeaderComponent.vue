241a4e7857fae34f03fbb6450c657493d444e895
tmdals <tmdals0145@naver.com>
2024-02-05 17:55:06 +0900

    2024/02/05

---
+++ src/layouts/HeaderComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="nav">
+        <router-link class="nav-link active" aria-current="page" to="#">Active</router-link>
+        <router-link class="nav-link" to="#">등록</router-link>
+        <router-link class="nav-link disabled" aria-disabled="true" to="#">Disabled</router-link>
+</nav>
+</template>
---
+++ src/views/UserListView.vue
@@ -0,0 +1,30 @@
+<!-- UserListView.vue-->
+<template>
+    <div class="container">
+        <h1>전체 회원 조회</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return {
+            userList : []
+        }
+    },
+    async created(){
+        await this.getUserList(); //비동기 작업.
+    },
+    methods : {
+        async getUserList(){
+            // 동기작업.
+            let result = await axios.get('http://localhost:3000')
+                              .catch(err => console.log(err));
+            let list = result.data;
+            this.userList = list;
+        }
+    }
+}
+</script>
